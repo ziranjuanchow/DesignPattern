d9cae4a719a0365a0df7381dfa24d428cff0513e
vo01github <hxt2014bbg@icloud.com>
2015-05-05 19:37:39 +0800

    1

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # DesignPattern
 Design Pattern
+
+
+## 单例模式 Singleton Design Pattern
+
---
+++ SingletonDesignPattern.cpp
@@ -0,0 +1,61 @@
+﻿
+
+// 单例模式-Singleton Design Pattern
+
+/*
+介绍：
+	 
+*/
+
+
+/*
+历史背景：
+  
+*/
+
+
+/*
+思路来源：
+	
+*/
+
+/*
+
+
+*/
+
+
+// __________________________0  __________________________
+
+
+// __________________________9 代码实现 __________________________
+/// .h ===================================================
+class EISceneManager
+{
+public:
+
+    static EISceneManager* sharedSceneManager();            // 单例模式
+
+private:
+    EISceneManager();
+    static EISceneManager* m_pSceneManager;
+      
+};
+
+
+/// .cpp ===================================================
+EISceneManager* EISceneManager::m_pSceneManager = NULL;
+
+EISceneManager::EISceneManager()
+{
+
+}
+
+EISceneManager* EISceneManager::sharedSceneManager()
+{
+    if(m_pSceneManager == NULL)
+    {
+        m_pSceneManager = new EISceneManager();
+    }
+    return m_pSceneManager;
+}
