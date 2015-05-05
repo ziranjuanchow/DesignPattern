ff7a0198930a967a114f53b693c90e9390f138e8
vo01github <hxt2014bbg@icloud.com>
2015-05-05 20:57:55 +0800

    2

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ Design Pattern
 
 ## 单例模式 Singleton Design Pattern
 
+
+
+## 简单工厂模式 Static Factory Method
--- StaticFactoryMethod.cpp
+++ StaticFactoryMethod.cpp
@@ -36,3 +36,89 @@
 
 
 /// .cpp ===================================================
+
+// 下面这些代码 是C# 的
+// __________________________ 01-版本 __________________________
+class Program
+{
+
+    static void Main(string[] args)
+    {
+        try
+        {
+            Console.Write("请输入数字A：");
+            string strNumberA = Console.ReadLine();
+
+            Console.Write("请选择运算符号(+,-,*,/):");
+            string strOperate = Console.ReadLine();
+
+            Console.Write("请输入数字B：");
+            string strNumberB = Console.ReadLine();
+
+            string strResult = "";
+            switch(strOperate)
+            {
+                case "+":
+                    strResult = Convert.ToString(Convert.ToDouble(strNumberA)
+                        + Convert.ToDouble(strNumberB));
+                    break;
+                case "-":
+                    strResult = Convert.ToString(Convert.ToDouble(strNumberA)
+                        - Convert.ToDouble(strNumberB)); 
+                    break; 
+                case "*":
+                    strResult = Convert.ToString(Convert.ToDouble(strNumberA)
+                        * Convert.ToDouble(strNumberB)); 
+                    break; 
+                case "/":
+                    if(strNumberB != "0" )
+                    strResult = Convert.ToString(Convert.ToDouble(strNumberA)
+                        / Convert.ToDouble(strNumberB)); 
+                    else
+                        strResult = "除数不能为0";
+                    break;            
+            }
+            Console.WriteLine("结果是：" + strResult);
+            Console.ReadLine();
+        }
+        catch(Exception ex)
+        {
+            Console.WriteLine("您的输入有错" + ex.Message);
+        }
+    }
+
+};
+
+
+// __________________________ 02- 业务的封装 版本 __________________________
+
+
+// Operation 运算类
+public class Operation
+{
+    public static double GetResult(double numberA,double numberB,string operate )
+    {
+        double result = 0d;
+        switch(operate)
+        {
+            case "+":
+                result = numberA + numberB;
+                break;
+            case "-":
+                result = numberA - numberB;
+                break; 
+            case "*":
+                result = numberA * numberB;
+                break; 
+            case "/":
+                if(strNumberB != "0" )
+                result = numberA / numberB; 
+                else
+                    strResult = "除数不能为0";
+                break;            
+        }
+        return result;
+    }
+};
+
+
