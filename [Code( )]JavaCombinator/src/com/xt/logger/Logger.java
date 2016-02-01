42231662941ec2758fc4ba0a080b7dc9b6a670dc
vo01github <hxt2014bbg@icloud.com>
2016-02-01 14:27:28 +0800

    6

---
+++ [Code( )]JavaCombinator/src/com/xt/logger/Logger.java
@@ -0,0 +1,10 @@
+package com.xt.logger;
+
+
+
+public class Logger {
+	public static void runTest() { 
+		System.out.println("logging test - 2016-02-01");
+        
+    }
+}
---
+++ [Code( )]JavaCombinator/src/com/xt/main/MainRun.java
@@ -0,0 +1,10 @@
+package com.xt.main;
+
+import com.xt.logger.Logger;
+
+public class MainRun {
+public static void main(String[] args) { 
+	  Logger  er = new Logger();
+	  er.runTest();
+	}
+}
